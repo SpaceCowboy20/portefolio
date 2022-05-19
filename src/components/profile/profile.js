fa419a9873451128341ffba4f0e05fd768ac3cae
SpaceCowboy20 <housseine18@gmail.com>
2022-05-19 19:31:33 +0100

    need alot correction comment in bottom of css file

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -4,13 +4,13 @@ export default function Profile() {
   return (
     <div className="profile">
       <div className="textbox">
+        <div className="borderpp">
+          <div className="cadre-pp">
+            <img src={require("../../imgs/pp.jpg")} alt="" className="pp" />
+          </div>
+        </div>
         <h1>Housseine FELLAHI</h1>
         <h4>à propos de moi</h4>
-      </div>
-      <div className="borderpp">
-        <div className="cadre-pp">
-          <img src={require("../../imgs/pp.jpg")} alt="" className="pp" />
-        </div>
       </div>
     </div>
   );
